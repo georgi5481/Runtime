4cd079ff1330a0df007cca16f9d9d395d5411893
Georgi <georgi5481@abv.bg>
2022-02-28 22:43:51 +0200

    conected with cmake

--- sdl_utils/SDLLoader.cpp
+++ sdl_utils/SDLLoader.cpp
@@ -9,20 +9,38 @@
 
 //3rd-party includes
 #include<SDL.h>
+#include<SDL_ttf.h>
+#include<SDL_image.h>
 
 //Own includes
 #include "sdl_utils/SDLLoader.h"
 
 int32_t SDLLoader::init(){
-	if(EXIT_SUCCESS != SDL_Init(SDL_INIT_VIDEO))
+	if(EXIT_SUCCESS != TTF_Init())
 	{
 		std::cerr << "SDL_Init failed. Reason:" << SDL_GetError() << std::endl;
 		return EXIT_FAILURE;
 	}
 
+	if(EXIT_SUCCESS != SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_AUDIO))
+	{
+		std::cerr << "SDL_Init failed. Reason:" << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	constexpr int32_t imgFlags = IMG_INIT_PNG;
+	if(! (IMG_Init(imgFlags) & imgFlags)){
+		std::cerr << "SDL image coul not be initialised! SDL_miage Error:" <<
+				IMG_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+
 	return EXIT_SUCCESS;
 }
 void SDLLoader::deinit(){
+
+	TTF_Quit;
 	SDL_Quit();
 
 }
