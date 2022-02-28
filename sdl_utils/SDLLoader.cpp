1c4861bd1d3413d9fd8e93b7c9fb790e0ab109c6
Georgi <georgi5481@abv.bg>
2022-02-28 21:39:52 +0200

    adding extra class

---
+++ sdl_utils/SDLLoader.cpp
@@ -0,0 +1,22 @@
+//Corresponding header
+#include "SDLLoader.h"
+
+//C system includes
+
+//C++ system includes
+#include<cstdint>
+
+//3rd-party includes
+
+//Own includes
+
+
+SDLLoader::SDLLoader() {
+	// TODO Auto-generated constructor stub
+
+}
+
+SDL_Loader::~SDLLoader() {
+	// TODO Auto-generated destructor stub
+}
+
