ad1d0e569a83a56a630ada9fe5a7353b2b0de3c9
Georgi <georgi5481@abv.bg>
2022-03-03 16:07:56 +0200

    Finally fixed the cmake error

--- sdl_utils/SDLLoader.cpp
+++ sdl_utils/SDLLoader.cpp
@@ -9,9 +9,9 @@
 
 //3rd-party includes
 #include <SDL.h>
-#include <SDL_ttf.h>
 #include <SDL_image.h>
 #include <SDL_mixer.h>
+#include <SDL_ttf.h>
 
 //Own includes
 #include "sdl_utils/SDLLoader.h"
